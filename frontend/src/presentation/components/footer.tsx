98e55a444fb3df20e6b4cba1e7ddb8c7c5d115c1
silva4dev <web4dev.lucas@outlook.com>
2024-12-16 17:14:17 -0300

    feat: :sparkles: add footer

---
+++ frontend/src/presentation/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { HeartIcon } from "@heroicons/react/24/solid";
+
+const Footer: React.FC = () => (
+  <footer className="bg-zinc-700 text-white p-4 text-end">
+    <p className="text-[16px] flex items-center justify-end">
+      <span className="mr-2">{`© ${new Date().getFullYear()}`}</span>
+       developed with <HeartIcon data-testid="heart-icon" className="h-5 w-5 text-red-500 mx-2" />
+       by <span className="font-bold m-2">Lucas Alves</span>
+    </p>
+  </footer>
+);
+
+export default Footer;
