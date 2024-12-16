f9a5470834bcb4ac0fb56feeb8de4b7b6398f243
silva4dev <web4dev.lucas@outlook.com>
2024-12-16 17:14:25 -0300

    feat: :sparkles: add navbar

---
+++ frontend/src/presentation/components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar: React.FC = () => (
+  <nav className="bg-white shadow-md text-black p-4">
+    <h1 className="text-lg font-bold">Solides</h1>
+  </nav>
+);
+
+export default Navbar;
+
+
