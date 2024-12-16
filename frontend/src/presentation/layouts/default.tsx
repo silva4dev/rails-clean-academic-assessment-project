9f613884564432331646adbc692bd1750e2397a8
silva4dev <web4dev.lucas@outlook.com>
2024-12-16 17:16:00 -0300

    feat: :sparkles: add default layout

---
+++ frontend/src/presentation/layouts/default.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/footer';
+import Navbar from "../components/navbar";
+
+const DefaultLayout: React.FC = () => (
+  <div className="flex flex-col min-h-screen">
+    <Navbar />    
+    <main className="flex-1 p-4 mt-5">
+      <Outlet />
+    </main>
+    <Footer />
+  </div>
+);
+
+export default DefaultLayout;
