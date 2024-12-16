6ffc3fbcfbc5a061ef7bbc065cfdd1157dfd21b7
silva4dev <web4dev.lucas@outlook.com>
2024-12-16 09:03:29 -0300

    feat: :sparkles: add history repository

---
+++ backend/app/layers/domain/repositories/history_repository.rb
@@ -0,0 +1,13 @@
+module Domain
+  module Repositories
+    class HistoryRepository
+      def get_histories_by_student(id)
+        raise NotImplementedError
+      end
+
+      def get_histories_by_reference_date(start_date, end_date)
+        raise NotImplementedError
+      end
+    end
+  end
+end
