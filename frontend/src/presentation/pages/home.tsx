ffde8e68631da35e1ef7ffe0c794a92a30691346
silva4dev <web4dev.lucas@outlook.com>
2024-12-16 17:16:10 -0300

    feat: :sparkles: add home page

---
+++ frontend/src/presentation/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-4xl mb-3">Welcome to the Home Page</h1>
+      <p className="text-xl">This is the starting point of your application. Feel free to explore!</p>
+    </div>
+  );
+};
+
+export default HomePage;
