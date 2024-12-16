6208ea2396824c20415adb83b0410d4637090b35
silva4dev <web4dev.lucas@outlook.com>
2024-12-16 09:00:07 -0300

    feat: :sparkles: add get histories input dto

---
+++ backend/app/layers/application/dtos/get_histories_input_dto.rb
@@ -0,0 +1,15 @@
+require_relative "../../shared_kernel/domain/validation"
+
+module Application 
+  module Dtos 
+    class GetHistoriesInputDto < Dry::Struct
+      attribute :student_id, SharedKernel::Domain::Types::String
+  
+      def to_h
+        { 
+          student_id: student_id
+        }
+      end
+    end
+  end
+end
