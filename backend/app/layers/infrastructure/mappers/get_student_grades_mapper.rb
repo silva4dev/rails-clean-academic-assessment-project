c2aafed8e1d3d0350c315e0463ab5946dd5c4643
silva4dev <web4dev.lucas@outlook.com>
2024-12-16 09:04:47 -0300

    feat: :sparkles: add get student grades mapper

---
+++ backend/app/layers/infrastructure/mappers/get_student_grades_mapper.rb
@@ -0,0 +1,17 @@
+require_relative "../../shared_kernel/infrastructure/mapper"
+require_relative "../../domain/entities/history_entity"
+require_relative "../../domain/entities/student_entity"
+
+module Infrastructure
+  module Mappers 
+    class GetStudentGradesMapper < SharedKernel::Infrastructure::Mapper
+      def self.to_entity(dao)
+        
+      end
+
+      def self.to_dao(entity)
+        
+      end
+    end
+  end
+end
