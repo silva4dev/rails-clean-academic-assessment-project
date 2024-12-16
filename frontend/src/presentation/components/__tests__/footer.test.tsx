0403646440fb24ffd63ab8f4f131c8b30d65adc7
silva4dev <web4dev.lucas@outlook.com>
2024-12-16 17:12:00 -0300

    test: :test_tube: add footer

---
+++ frontend/src/presentation/components/__tests__/footer.test.tsx
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+import Footer from "../footer";
+
+describe('Footer', () => {
+  test('should render the footer content with the current year and correct text', () => {
+    const currentYear = new Date().getFullYear();
+    const { getByText } = render(<Footer />);
+    expect(getByText(`© ${currentYear}`)).toBeInTheDocument();
+    expect(getByText(/developed with/i)).toBeInTheDocument();
+    expect(getByText('Lucas Alves')).toBeInTheDocument();
+  });
+
+  test('should render the heart icon with correct color', () => {
+    const { getByTestId } = render(<Footer />);
+    const heartIcon = getByTestId('heart-icon');
+    expect(heartIcon).toBeInTheDocument();
+    expect(heartIcon).toHaveClass('text-red-500');
+  });
+});
