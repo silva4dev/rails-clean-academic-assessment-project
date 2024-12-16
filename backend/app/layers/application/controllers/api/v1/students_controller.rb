af85721a39ccb3393ec23a81d42bccb9978886b3
silva4dev <web4dev.lucas@outlook.com>
2024-12-16 08:59:29 -0300

    feat: :sparkles: add students controller

---
+++ backend/app/layers/application/controllers/api/v1/students_controller.rb
@@ -0,0 +1,30 @@
+require_relative "../../../../shared_kernel/application/application_controller"
+require_relative "../../../services/get_top_students_service"
+require_relative "../../../dtos/get_top_students_input_dto"
+
+module Application
+  module Controllers
+    module Api 
+      module V1 
+        class StudentsController < SharedKernel::Application::ApplicationController
+          def index 
+            input_dto = Dtos::GetTopStudentsInputDto.new(
+              start_date: (Date.today - 1.month).beginning_of_month,
+              end_date: (Date.today - 1.month).end_of_month
+            )
+            result = Services::GetTopStudentsService.new.execute(input_dto)
+            if result.success? 
+              render json: { 
+                data: result.success[:histories]
+              }, status: result.success[:code]
+            else 
+              render json: { 
+                error: result.failure[:error] 
+              }, status: result.failure[:code] 
+            end
+          end
+        end
+      end
+    end
+  end
+end
