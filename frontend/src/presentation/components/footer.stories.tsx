069971bc3571b72eb164ead09789912bda121c6e
silva4dev <web4dev.lucas@outlook.com>
2024-12-16 21:37:39 -0300

    feat: :sparkles: add footer stories

---
+++ frontend/src/presentation/components/footer.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Footer from "./footer";
+
+const meta: Meta<typeof Footer> = {
+  title: "Components/Footer",
+  component: Footer,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Footer>;
+export const Default: Story = {};
