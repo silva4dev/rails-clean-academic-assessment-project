80f648c099ff3fd54c0f1bbcd7912c573365e86a
silva4dev <web4dev.lucas@outlook.com>
2024-12-18 01:50:23 -0300

    feat: :sparkles: change button stories

--- frontend/src/presentation/components/button.stories.tsx
+++ frontend/src/presentation/components/button.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 import Button from "./button";
+import { BrowserRouter } from "react-router-dom";
 
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
@@ -8,8 +9,27 @@ const meta: Meta<typeof Button> = {
   parameters: {
     layout: "fullscreen",
   },
+  argTypes: {
+    studentId: {
+      control: "text",
+      description: "ID of the student to link to.",
+      defaultValue: "123",
+    },
+  },
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
-export const Default: Story = {};
+
+export const Default: Story = {
+  args: {
+    studentId: "123",
+  },
+};
