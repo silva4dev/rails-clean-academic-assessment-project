3fa1157085298a289963eaa5d79da4b5df7462fb
silva4dev <web4dev.lucas@outlook.com>
2024-12-18 00:17:44 -0300

    feat: :sparkles: change footer component

--- frontend/src/presentation/components/footer.tsx
+++ frontend/src/presentation/components/footer.tsx
@@ -4,8 +4,7 @@ import { HeartIcon } from "@heroicons/react/24/solid";
 const Footer: React.FC = () => (
   <footer className="bg-zinc-700 text-white p-4 text-end">
     <p className="text-[16px] flex items-center justify-end">
-      <span className="mr-2">{`© ${new Date().getFullYear()}`}</span>
-       developed with <HeartIcon data-testid="heart-icon" className="h-5 w-5 text-red-500 mx-2" />
+       Developed with <HeartIcon data-testid="heart-icon" className="h-5 w-5 text-red-500 mx-2" />
        by <span className="font-bold m-2">Lucas Alves</span>
     </p>
   </footer>
