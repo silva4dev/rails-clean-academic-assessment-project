97bb0ccd5785163670a9a7dd868865063c83b1cf
silva4dev <web4dev.lucas@outlook.com>
2024-12-18 00:22:21 -0300

    feat: :sparkles: add get histories service

--- frontend/src/domain/services/get-histories-service.ts
+++ frontend/src/domain/services/get-histories-service.ts
@@ -1,13 +1,9 @@
 import { Either, Service } from "../../shared_kernel";
 import { HistoryEntity } from "../entities";
 
-export type Request = {
-  name: string;
-  capacity: number;
-  location: string;
-};
-
-export type GetHistoriesResponse = Either<null, HistoryEntity>;
+export type GetHistoriesResponse = Either<null, {
+  data: HistoryEntity[]
+}>;
 
 export interface GetHistoriesService
   extends Service<null, GetHistoriesResponse> {}
