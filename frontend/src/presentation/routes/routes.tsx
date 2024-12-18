72fcb4bf0b3bc8886deeced2a3894710de4c6505
silva4dev <web4dev.lucas@outlook.com>
2024-12-18 00:17:59 -0300

    feat: :sparkles: add student route

--- frontend/src/presentation/routes/routes.tsx
+++ frontend/src/presentation/routes/routes.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/home';
 import DefaultLayout from '../layouts/default';
+import StudentPage from '../pages/student';
 
 const AppRoutes: React.FC = () => (
   <Routes>
     <Route element={<DefaultLayout />}>
       <Route path="/" element={<HomePage />} />
+      <Route path="/student/:id" element={<StudentPage />} />
     </Route>
   </Routes>
 );
